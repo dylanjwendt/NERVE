3894632d87a6c2b3f259f65f880e2adaac6ba4b6
ktucalvin <ktucalvin@gmail.com>
2021-03-20 13:34:20 -0600

    Add constructor, private variable, named export example

--- Engine/src/helper.ts
+++ Engine/src/helper.ts
@@ -1,5 +1,15 @@
 export default class Helper {
+  #private = 3;
+
+  constructor() {
+    this.#private = 4;
+  }
+
   static getGreeting() {
     return 'Hello World!';
   }
+}
+
+export function hi() {
+  console.log('hi');
 }
