4f24f909e58423b718b24229a79146f6dd98158b
Jordan <36747938+Jordantayl@users.noreply.github.com>
2021-11-22 19:59:03 -0700

    Added and updated server comments on Server

--- Server/src/colyseus/game-state.ts
+++ Server/src/colyseus/game-state.ts
@@ -1,5 +1,8 @@
 import { Schema, type } from "@colyseus/schema";
 
+/**
+ * Represents the state of the game.
+ */
 export class GameState extends Schema {
     @type("string")
     public text = "";
